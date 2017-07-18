b38caa0dc82a4796fa359fa8a22b482b2d860bb0
Martin <martin@Martinui-MacBook-Pro.local>
2017-07-18 18:12:48 +0900

    코드 리팩토링

--- public/module/crudBoard.js
+++ public/module/crudBoard.js
@@ -4,36 +4,51 @@ define([
 	,'crudBoardApi'
 ], function($,handlebars, getApiData){
 	var bindTable = (function(){
-		var wrapper;
-		var bindTarget;
-		var bindButton;
+		var obj;
+		var OPTIONS = {
+			IS_BIND_DATA : false
+		}
 		var INDEX;
 
 		function init(options){
-			wrapper = $(options.wrapper);
-			bindTarget=$(options.bindTarget);
-			bindButton=$(options.bindButton);
-
+			bindSelector(options);
 			bindEvents();
-		}
-
-		function bindEvents(){
-			bindButton.on("click", insertData);
 			getData();
 		}
 
-		function getData(){
-			if(bindTarget.children("tr").size()>0){
-				bindTarget.children("tr").remove();
+		function bindSelector(options){
+			obj = {
+				wrapper : $(options.wrapper),
+				bindTarget : $(options.bindTarget),
+				bindButton : $(options.bindButton),
+				creatorIdTextArea : $("#creator_id"),
+				titleTextArea : $("#title"),
+				contentTextArea : $("#content")
 			}
+		}
 
-			var dummyDom = wrapper.html();
+		function bindEvents(){
+			obj.bindButton.on("click", insertData);
+			$(document).on("click","[data-module-btn='edit']", function(){
+				setEachData($(this));
+			});
+			$(document).on("click", "[data-module-btn='delete']", function(){
+				deleteUserBoardData($(this));
+			})
+		}
+
+		function getData(){
+			//데이터가 바인딩 되어있으면 OPTIONS.IS_BIND_DATA가 true
+			if(OPTIONS.IS_BIND_DATA === true){
+				obj.bindTarget.children("tr").remove();
+			}
+			var dummyDom = obj.wrapper.html();
 			var template= handlebars.compile(dummyDom);
 			var dataItem = template({
-				"boardData" :getApiData.callDataApi("GET","/api/Board","JSON")
+				"boardData" : getApiData.callDataApi("GET","/api/Board","JSON")
 			});
-			bindTarget.append(dataItem);
-			dynamicInit().bindEvents();
+			obj.bindTarget.append(dataItem);
+			OPTIONS.IS_BIND_DATA = true;
 		}
 
 		function insertData(){
@@ -53,57 +68,25 @@ define([
 			}
 			INDEX=undefined;
 			getData();
-		}
-		var dynamicInit = function(){
-			var editButton = $("[data-module-btn='edit']");
-			var deleteButton = $("[data-module-btn='delete']");
-			var creator_id = $("#creator_id");
-			var title = $("#title");
-			var content = $("#content");
-			function bindEvents(){
-				editButton.on("click", function(){
-					setEachData($(this));
-				});
-				deleteButton.on("click", function(){
-					deleteFunction($(this));
-				});
-			}
-			function deleteFunction(me){
-				var parent = me.closest("[data-module-idx]");
-				$.ajax({
-					type:"POST",
-					url:"/delete/Board",
-					dataType:"JSON",
-					data:{
-						'idx':parent.data("module-idx")
-					},
-					complete:function(){
-						console.log(11);
-						getData();
-					},
-					success:function(){
-						console.log(2);
-					}
-				})
-			}
-			function setEachData(me){
-				var parents =me.closest("[data-module-idx]");
-				var creatorIdContent = parents.find("[data-module-contents='creator_id']").text();
-				var titleContent = parents.find("[data-module-contents='title']").text();
-				var contentContent = parents.find("[data-module-contents='content']").text();
-				INDEX = parents.data("module-idx");
-
-				creator_id.val(creatorIdContent);
-				title.val(titleContent);
-				content.val(contentContent);
-			}
-			function makeDim(){
-				bindTarget.parent("table").append("<div class='dim'></div>");
-			}
-			return {
-				bindEvents:bindEvents
-			}
 		};
+		function setEachData(me){
+			var parents = me.closest("[data-module-idx]");
+			var creatorIdContent = parents.find("[data-module-contents='creator_id']").text();
+			var titleContent = parents.find("[data-module-contents='title']").text();
+			var contentContent = parents.find("[data-module-contents='content']").text();
+			INDEX = me.data("temp-index");
+
+			obj.creatorIdTextArea.val(creatorIdContent);
+			obj.titleTextArea.val(titleContent);
+			obj.contentTextArea.val(contentContent);
+		};
+		function deleteUserBoardData(me){
+			var deleteTarget = me.data("delete-target-index");
+			getApiData.callDataApi("POST","/delete/Board","Text", {
+				'idx':deleteTarget
+			});
+			getData();
+		}
 
 		return{
 			init: init
